cc381d6871fc74b43f0b507f730b3fdfbc93ec6c
Shola Ayeni <ayenisholah@yahoo.com>
2019-05-16 12:33:57 +0100

    prototype-refactor completed

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -30,3 +30,64 @@ class Humanoid extends CharacterStats {
         return `${this.name} offers a greeting in ${this.language}`;
     }
 }
+
+const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 1,
+      height: 1,
+    },
+    healthPoints: 5,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+      'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+});
+
+const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2,
+    },
+    healthPoints: 15,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+      'Giant Sword',
+      'Shield',
+    ],
+    language: 'Common Tongue',
+});
+
+const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    healthPoints: 10,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+      'Bow',
+      'Dagger',
+    ],
+    language: 'Elvish',
+});
+
+console.log(mage.createdAt);
+console.log(archer.dimensions);
+console.log(swordsman.healthPoints);
+console.log(mage.name);
+console.log(swordsman.team);
+console.log(mage.weapons);
+console.log(archer.language);
+console.log(archer.greet());
+console.log(mage.takeDamage());
+console.log(swordsman.destroy());
