cd22a2e20410729ff86e2b30583d35e2e30f3c5d
Shola Ayeni <ayenisholah@yahoo.com>
2019-05-16 12:59:55 +0100

    Instructed Class created

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -8,3 +8,18 @@ class Person {
         return `Hello my name is ${this.name}, I am from ${this.location}`
     }
 }
+
+class Instructor extends Person {
+    constructor(attributes){
+        super(attributes);
+        this.specialty = attributes.specialty;
+        this.favLanguage = attributes.favLanguage;
+        this.catchPhrase = attributes.catchPhrase;
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}`
+    }
+    grade(Student, subject){
+        return `${Student.name} receives a perfect score on ${subject}`
+    }
+}
